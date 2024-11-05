3281f186218cbbe4d712ef98ef8f5f76e1b2e1e7
Shamso Osman <osmanshamso004@gmail.com>
2024-11-05 19:12:31 +0000

    refactor fizzbuzz challenge

--- S-Osman4/fizzbuzz.py
+++ S-Osman4/fizzbuzz.py
@@ -1,16 +1,32 @@
-def fizzbuzz():
-    '''
-    Print numbers from 1 to 100.
-    For multiples of 3, print Fizz.
-    For multiples of 5, print Buzz.
-    For multiples of both 3 and 5, print FizzBuzz.
-    '''
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
+#!/usr/bin/env python3
+
+def fizzbuzz(start, end):
+    # Plan:
+    # 1. Loop through each number from start to end (inclusive).
+    # 2. For each number, check:
+    #    - If it is divisible by 3 and 5, print "FizzBuzz".
+    #    - If it is divisible by 3, print "Fizz".
+    #    - If it is divisible by 5, print "Buzz".
+    #    - Otherwise, print the number itself.
+
+    for number in range(start, end + 1):  # Loop from start to end (inclusive)
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")  # Divisible by both 3 and 5
+        elif number % 3 == 0:
+            print("Fizz")  # Divisible by 3
+        elif number % 5 == 0:
+            print("Buzz")  # Divisible by 5
         else:
-            print(i)
+            print(number)  # Print the number itself
+
+def main():
+    # Set the range for FizzBuzz
+    start = 1  # Starting number
+    end = 100  # Ending number
+
+    print(f"FizzBuzz from {start} to {end}:")
+    fizzbuzz(start, end)  # Call the fizzbuzz function with the specified range
+
+# Run the main function
+if __name__ == "__main__":
+    main()
